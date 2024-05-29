32baa50db6a91f3c36964379bbbf24c9a28cdcaa
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-05-29 03:18:44 +0300

    Time: 38 ms (31.53%) | Memory: 16.5 MB (80.41%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -1,30 +1,11 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        # var holds count of letters of last word in string
-        counter = 0
-        # var verfies if we skipped the last white-spaces
-        flag = True
-        # var to reverse-traverse through string
-        reversed_index = -1
 
-        for i in range(len(s)):
-            
-            # verify if reached the last word or not
-            if (flag and s[reversed_index] == " "):
-                reversed_index -=1
-                continue
-            
-            # reached the last word in string 
-            flag = False            
-                
-            # increment the counter since we caught the last word
-            if (not s[reversed_index] == " "):
-                counter +=1
-            
-            
-            # verify if we reached the end of the last word
-            if (s[reversed_index] == " " or i + 1 == len(s)):
-                return counter
-                
-            # decrement the index to move to the next letter
-            reversed_index -=1
+        # remove leading and trailing spaces
+        s = s.strip()
+
+        # Find the index of the last space
+        last_space_index = s.rfind(" ")
+
+        # Return the length of the last word
+        return (len(s) - 1) - last_space_index
