abbbe8c7085fb9ab51e14fb776b2ad0bb6306b1c
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-11-19 13:44:14 +0200

    Time: 12 ms (9.65%) | Memory: 16.8 MB (13.18%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,37 +1,13 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        sum = 0
+        roman = {"I": 1, "V": 5, "X": 10, "L":50, "C": 100, "D": 500, "M":1000}
+        res = 0
+        
         for i in range(len(s)):
-            char = s[i]
-            next_char = "placeholder"
-            if(i < len(s) - 1):
-                next_char = s[i+1]
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+                res-= roman[s[i]]
+            else:
+                res+= roman[s[i]]
+        return res
 
-            if (char == "I"):
-                sum+= 1
-                if(next_char == "V" or next_char == "X"):
-                    sum-= 2
-
-            elif (char == "V"):
-                sum+= 5
-
-            elif (char == "X"):
-                sum+=10
-                if(next_char == "L" or next_char == "C"):
-                    sum-= 20
-
-            elif (char == "L"):
-                sum+=50
-
-            elif (char == "C"):
-                sum+=100
-                if(next_char == "D" or next_char == "M"):
-                    sum-= 200
-
-            elif (char == "D"):
-                sum+=500
-
-            elif (char == "M"):
-                sum+=1000
-
-        return sum
+# Explanation Video (https://www.youtube.com/watch?v=3jdxYj3DD98)
